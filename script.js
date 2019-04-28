1d1ef6da6e28aec74d05bcdf7d5ef065e8397b29
Sebastian A Colombini <colombinis@gmail.com>
2019-04-28 11:54:30 -0300

    Registra notification

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-        .then((reg) => { console.log('se registro sw ok', reg) })
-        .catch((err) => { console.warn('Error al registrar sw', err) })
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js')
+            .then((reg) => { console.log('se registro sw ok', reg) })
+            .catch((err) => { console.warn('Error al registrar sw', err) })
+    });
+}
+
+
+if (window.Notification ) {
+    debugger
+    Notification.requestPermission(status => {
+        console.log('requestPermission - status',status);
+        const n = new Notification('My Titulo',{
+            body:'Sera la notificacion ? :)',
+            icon:'./img/ico_36.png'
+        });
+    });
 }
