5a62ec09e4df787dd243ce88a87d3c24815d9b61
Sebastian A Colombini <colombinis@gmail.com>
2019-04-28 11:25:41 -0300

    remove bad push event

--- sw.js
+++ sw.js
@@ -87,19 +87,3 @@ function mycacheAcum(req) {
         }
     );
 }
-
-/* push */
-
-self.addEventListener('push', function(event) {  
-    var title = 'Yay a message.';  
-    var body = 'We have received a push message.';  
-    var icon = '/images/smiley.svg';  
-    var tag = 'simple-push-example-tag';
-    event.waitUntil(  
-      self.registration.showNotification(title, {  
-        body: body,  
-        icon: icon,  
-        tag: tag  
-      })  
-    );  
-  });
